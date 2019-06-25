b818e84c1c6be01a098cb23a7aded53ce8246244
Ryan HUTCHINSON <rhutchin@c4r21s4.wethinkcode.co.za>
2019-06-25 12:03:18 +0200

    stuff

--- main_test_fd.c
+++ main_test_fd.c
@@ -6,7 +6,7 @@
 /*   By: rhutchin <rhutchin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 12:09:18 by rhutchin          #+#    #+#             */
-/*   Updated: 2019/06/25 11:58:55 by rhutchin         ###   ########.fr       */
+/*   Updated: 2019/06/25 12:02:47 by rhutchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,7 @@ int main(int ac, char **av)
 	if(ac < 1)
 		return (0);
 	fd = open(av[1], O_RDONLY);
-	/* while(get_next_line(fd, &line) > 0)
-	{
-		ft_putstr(line);
-		ft_putchar('\n');
-		i++;
-	}*/
-
-	if (get_next_line(fd, &line) > 0)
-	{
-		ft_putstr(line);
-		ft_putchar('\n');
-		i++;
-	}
-	if (get_next_line(fd, &line) > 0)
+	while(get_next_line(fd, &line) > 0)
 	{
 		ft_putstr(line);
 		ft_putchar('\n');
